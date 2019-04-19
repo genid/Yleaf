d63053082de0014cf101e6312472d40d11a864f8
diego montiel <diegoomontiel@gmail.com>
2019-04-19 15:07:10 +0200

    remove r scripts

--- install.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-
-packages <- c("mice", "plyr", "foreach")
-if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())))  
-}else {
-  message("All required R packages are already installed, cool!")
-}
