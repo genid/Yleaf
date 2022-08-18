e95da8e3b5b9315a3679b8d842393575a3b42483
bram <b.vanwersch@erasmusmc.nl>
2022-08-18 12:25:21 +0200

    added way of drawing a set of haplogroups in the haplogroup tree

--- create_haplogroup_tree.py
+++ create_haplogroup_tree.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Set
 import graphviz
 from collections import defaultdict
 import argparse
@@ -9,8 +9,13 @@ from tree import Tree
 def main():
     namespace = get_arguments()
     haplogroups, sample_mapping = read_input_file(namespace.input)
+    add_main_haplogroups(haplogroups)
     partial_haplogroup_dict = haplogroup_tree_dict(haplogroups)
-    make_dendrogram(partial_haplogroup_dict, sample_mapping, namespace.outfile)
+    if namespace.collapse_mode:
+        edge_mapping = collapse_tree_dict(partial_haplogroup_dict, sample_mapping)
+    else:
+        edge_mapping = {}
+    make_dendrogram(partial_haplogroup_dict, sample_mapping, edge_mapping, namespace.outfile)
 
 
 def get_arguments() -> argparse.Namespace:
@@ -18,9 +23,14 @@ def get_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Erasmus MC: Genetic Identification\n Haplogroup tree creation")
 
     parser.add_argument("-i", "--input", required=True,
-                        help="prediction file (.hg file) containing the predicted haplogroups for different samples",
+                        help="prediction file (.hg file) containing the predicted haplogroups for different samples. "
+                             "Alternalively you can provide a file containing samples in the first column and "
+                             "haplogroups in the second.",
                         metavar="FILE")
-
+    parser.add_argument("-c", "--collapse_mode", help="If you add this flag all the nodes outside the main and final"
+                                                      " nodes will be compressed together creating a signigicantly "
+                                                      "smaller picture",
+                        action="store_true")
     parser.add_argument("-o", "--outfile", required=True, help="Output file name", metavar="FILE")
 
     args = parser.parse_args()
@@ -42,14 +52,20 @@ def read_input_file(
             haplogroup = haplogroup.split("*")[0]
             haplogroups.append(haplogroup)
             sample_mapping[haplogroup].append(sample)
-    return haplogroups, sample_mapping
+    return haplogroups, dict(sample_mapping)
+
+
+def add_main_haplogroups(haplogroups: List[str]):
+    haplogroups.extend([chr(nr) for nr in range(66, 85)])
+    haplogroups.append("A0-T")
+    haplogroups.append("A00")
 
 
 def haplogroup_tree_dict(
     haplogroups: List[str]
-) -> Dict[str, List[str]]:
+) -> Dict[str, Set[str]]:
     tree = Tree("Hg_Prediction_tables/tree.json")
-    partial_tree_dict = defaultdict(list)
+    partial_tree_dict = defaultdict(set)
     for name in haplogroups:
         path = []
         node = tree.get(name)
@@ -59,14 +75,63 @@ def haplogroup_tree_dict(
         parent = path.pop()
         while len(path) > 0:
             child = path.pop()
-            partial_tree_dict[parent].append(child)
+            partial_tree_dict[parent].add(child)
             parent = child
-    return partial_tree_dict
+    return dict(partial_tree_dict)
+
+
+def collapse_tree_dict(
+    partial_tree_dict: Dict[str, Set[str]],
+    sample_mapping: Dict[str, List[str]]
+) -> Dict[Tuple[str, str], str]:
+    edge_mapping = {}
+    remove_keys = set()
+    for parent, children in partial_tree_dict.items():
+        if parent in remove_keys:
+            continue
+        orig_parent = parent
+        for child in children.copy():
+            total_collapsed = 0
+            partial_tree_dict[orig_parent].remove(child)
+            while True:
+                if not can_collapse(child, partial_tree_dict, sample_mapping):
+                    break
+                remove_keys.add(child)
+                child = next(iter(partial_tree_dict[child]))
+                total_collapsed += 1
+            partial_tree_dict[orig_parent].add(child)
+            if total_collapsed > 0:
+                edge_mapping[(orig_parent, child)] = str(total_collapsed)
+    # remove all the collapsed nodes
+    for name in remove_keys:
+        del partial_tree_dict[name]
+    return edge_mapping
+
+
+def can_collapse(
+    name: str,
+    partial_tree_dict: Dict[str, Set[str]],
+    sample_mapping: Dict[str, List[str]]
+) -> bool:
+    # is only a child
+    if name not in partial_tree_dict:
+        return False
+    # more then one child
+    if len(partial_tree_dict[name]) != 1:
+        return False
+    # major node
+    if name == 'A0-T' or '-' not in name:
+        return False
+    # node linked to sample
+    if name in sample_mapping:
+        return False
+    return True
 
 
 def make_dendrogram(
-    partial_tree_dict: Dict[str, List[str]],
+    partial_tree_dict: Dict[str, Set[str]],
     sample_mapping: Dict[str, List[str]],
+    edge_mapping: Dict[Tuple[str, str], str],
     output_file: str
 ):
     dot = graphviz.Digraph()
@@ -81,9 +146,14 @@ def make_dendrogram(
             if child not in covered_nodes:
                 dot.node(child, child, shape="box")
                 covered_nodes.add(child)
-            if (parent, child) not in covered_edges:
-                dot.edge(parent, child, weight="1")
-                covered_edges.add((parent, child))
+            edge_name = (parent, child)
+            if edge_name not in covered_edges:
+                if edge_name in edge_mapping:
+                    edge_label = edge_mapping[edge_name]
+                else:
+                    edge_label = ''
+                dot.edge(parent, child, weight="1", label=edge_label)
+                covered_edges.add(edge_name)
                 if child in sample_mapping:
                     dot.node('\n'.join(sample_mapping[child]), '\n'.join(sample_mapping[child]), shape="box",
                              style='filled', fillcolor="green")
