17ca7af5c4a03b6222fa2cfeca9cd6d38959bd12
bram <b.vanwersch@erasmusmc.nl>
2022-08-29 12:56:07 +0200

    created dummy files to ensure that these files are removed when uninstalling

--- yleaf/Yleaf.py
+++ yleaf/Yleaf.py
@@ -298,7 +298,7 @@ def check_reference(
     requested_version: str,
 ):
     reference_file = get_reference_path(requested_version, True)
-    if not reference_file.exists():
+    if not os.path.getsize(reference_file) == 0:
         LOG.info(f"No reference genome version was found. Downloading the {requested_version} reference genome. This "
                  f"should be a one time thing.")
         download_reference.main(requested_version)
